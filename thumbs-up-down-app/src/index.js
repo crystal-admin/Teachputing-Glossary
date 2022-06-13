7246cc1cc2b091c178206cb2eb28c03fb59b5732
Faytey7 <faithroberts708@gmail.com>
2022-06-13 16:19:37 +0100

    new changes

--- thumbs-up-down-app/src/index.js
+++ thumbs-up-down-app/src/index.js
@@ -2,57 +2,37 @@ import React, { Component } from "react";
 import ReactDOM from 'react-dom/client';
 import "./App.css";
 
-const reduceOne = (prevState, groupName, otherGroupName) => {
-  prevState[groupName].wasClicked
-    ? prevState[groupName].count = prevState[groupName].count - 1
-    : prevState[groupName].count = prevState[groupName].count + 1;
-  prevState[groupName].wasClicked = !prevState[groupName].wasClicked;
-  if (prevState[otherGroupName].wasClicked) {
-    prevState[otherGroupName].count = prevState[otherGroupName].count - 1;
-    prevState[otherGroupName].wasClicked = false;
+class Liker extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      count: 0
+    };
+    this.likes = this.likes.bind(this);
+    this.disLikes = this.disLikes.bind(this);
   }
-  return prevState;
-};
+  likes(){
+    this.setState(state =>({
+      count: state.count + 1
+    }))
+  }
 
-const reducer = action =>
-  (prevState, props) =>
-    action.type === "TOGGLE_LIKE"
-      ? reduceOne(prevState, "like", "dislike")
-      : reduceOne(prevState, "dislike", "like");
+    disLikes(){
+      this.setState(state =>({
+        count: state.count - 1
+      }))
+  }
 
-class App extends Component {
-  state = {
-    like: {
-      count: 0,
-      wasClicked: false,
-    },
-    dislike: {
-      count: 0,
-      wasClicked: false,
-    },
-  };
-
-  toggleLike = () => this.setState(reducer({ type: "TOGGLE_LIKE" }));
-  toggleDislike = () => this.setState(reducer({ type: "TOGGLE_DISLIKE" }));
-};  
-
-export default Likes(); {
-  return(
-    <div className="App">
-      <header className="App-header">
-        <h1 className="App-title">Like - Dislike</h1>
-      </header>
-      <p className="App-intro">
-        <button className="like-button" onClick={this.toggleLike}>
-          👍 | {this.state.like.count}
-        </button>
-        <button className="dislike-button" onClick={this.toggleDislike}>
-          👎 | {this.state.dislike.count}
-        </button>
-      </p>
-    </div>
-  );
-};
+  render(){
+    return (
+      <div className="Liker">
+        <button onClick={this.likes}>👍</button>
+        <button onClick={this.disLikes}>👎</button>
+        <p>Current Likes: {this.state.count}</p>
+      </div>
+    )
+  }
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<Likes />);
+root.render(<Liker />);
